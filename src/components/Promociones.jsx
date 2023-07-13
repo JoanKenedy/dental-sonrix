68bf6af29a9b38d69858042fe317451e29a9c239
JoanKenedy <joankenedi9311@gmail.com>
2023-07-13 12:51:54 -0600

    promo-img

--- src/components/Promociones.jsx
+++ src/components/Promociones.jsx
@@ -3,16 +3,18 @@ import promocion1 from "../assets/blanqueamiento-cuadrado-dentix.jpg";
 import promocion2 from "../assets/limpieza-cuadrado-sonrix.jpg";
 import "../styles/promociones.css";
 
-const Promociones = ()  => {
-    return(
-        <div className="promo">
-            <div className="title"><h2>Promociones</h2></div>
-            <section className="promociones">
-                <img src="/src/assets/blanqueamiento-cuadrado-dentix.jpg" alt="" className="promotion-img" />
-                <img src="/src/assets/limpieza-cuadrado-sonrix.jpg" alt="" className="promotion-img" />
-            </section>
-        </div>
-    );
+const Promociones = () => {
+  return (
+    <div className="promo">
+      <div className="title">
+        <h2>Promociones</h2>
+      </div>
+      <section className="promociones">
+        <img src={promocion1} alt="" className="promotion-img" />
+        <img src={promocion2} alt="" className="promotion-img" />
+      </section>
+    </div>
+  );
 };
 
 export default Promociones;
