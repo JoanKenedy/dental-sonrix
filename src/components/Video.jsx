0fa51320c513a787c3da71dd0d26cc3e858d541b
Leonel <leonelvalentinn@gmail.com>
2024-01-18 15:03:02 -0600

    Video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/video.css";
 import videosonrix from "../assets/Somos-dental-sonrix.mp4";
-import poster from "../assets/poster-video.png";
+import poster from "../assets/poster.png";
 
 
 const Video = () => {
 
   return (
     <div className="video-container">
+      <h3>Conoce más sobre nosotros</h3>
       <video src={videosonrix} controls poster={poster}></video>
     </div>
   );
